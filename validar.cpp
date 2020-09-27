bbbb9f3a4407c778fd229719385c5b7b87946582
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-27 16:17:03 -0500

    Validar expresiones con if

--- validar.cpp
+++ validar.cpp
@@ -6,16 +6,17 @@ int main()
     float x, y, res, ycuadrada;
     std:: cout << "Ingresa la el valor para:\n";
     std:: cout << "x: "; std:: cin >> x;
+    ingresaydenuevo:
     std:: cout << "y: "; std:: cin >> y;
     ycuadrada=(pow(y,2)-1);
-
+    
     if (ycuadrada <= 0)
     {
+        std:: cout << "No se puede realizar esa operacion\n";
         std:: cout << "El resultado no es correcto, intentalo de nuevo";
+        goto ingresaydenuevo;
     }
-
     res = (sqrt(x))/ ycuadrada;
-
     std:: cout << "El resultado es: " << res;
 
     system("pause");
